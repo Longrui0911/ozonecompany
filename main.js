eb6499775d2b1a2cd924f0ec7850779b09998838
Longrui0911 <81362500+Longrui0911@users.noreply.github.com>
2021-09-25 15:26:27 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.client__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    adaptiveHeight: true
+});
